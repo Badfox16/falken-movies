71952ca19df9952f0c0b8ed1efddf7c064e59f0e
Mutizo de J. Maita <kingbadfox3@gmail.com>
2024-10-09 15:49:39 +0200

    Config inicial com navegacao e tailwind

---
+++ app/screens/TelaHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function TelaHome() {
+  return (
+    <View>
+        <Text>Home</Text>
+    </View>
+  )
+}
+
+export default TelaHome
